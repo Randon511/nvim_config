c1a5a5f1e58b22d89a86c24ab17edf0c1f02fef6
Randon <randon551@gmail.com>
2023-10-28 20:04:46 -0700

    Setting up nvim with terminal

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,27 +1,27 @@
 local lsp = require('lsp-zero')
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-	'clangd',
-	'cmake',
-	'lua_ls',
-    'pyright',
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        'clangd',
+        'cmake',
+        'lua_ls'
+    }
 })
 
-lsp.nvim_workspace()
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
+-- local cmp = require('cmp')
+-- local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+--   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+--   ["<C-Space>"] = cmp.mapping.complete(),
+-- })
+-- 
+-- lsp.setup_nvim_cmp({
+--   mapping = cmp_mappings
+-- })
 
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
--- init.lua
+++ init.lua
@@ -1,1 +1,31 @@
 require("randon")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {"nvim-lua/plenary.nvim", name="Plenary"},
+    {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+    {"ThePrimeagen/harpoon", name = "Harpoon"},
+    {"mbbill/undotree", name = "UndoTree"},
+    {"folke/todo-comments.nvim"},
+    {'nvim-telescope/telescope.nvim', tag = '0.1.4'},
+    {"williamboman/mason.nvim"},
+    {"williamboman/mason-lspconfig.nvim"},
+    {"neovim/nvim-lspconfig"},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'}
+})
